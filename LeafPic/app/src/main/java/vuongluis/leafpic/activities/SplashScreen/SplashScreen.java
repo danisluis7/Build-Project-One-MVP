49409570d2c879df979a2fe100d8466858220321
danisluis7 <lorence@enclave.vn>
2017-06-17 22:47:50 +0700

    Initialize project android

---
+++ LeafPic/app/src/main/java/vuongluis/leafpic/activities/SplashScreen/SplashScreen.java
@@ -0,0 +1,15 @@
+package vuongluis.leafpic.activities.SplashScreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vuongluis.leafpic.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
